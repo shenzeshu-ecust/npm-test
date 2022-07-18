293d5c3d6233b367aadad7b583423c4f167c2ba2
shenzeshu <zs.shen@trip.com>
2022-07-18 19:20:19 +0800

    day2

---
+++ learnVue3/hello-vue3/src/App.vue
@@ -0,0 +1,127 @@
+<script >
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import Content from './components/Content.vue'
+import { ref, reactive, toRefs, watch, watchEffect, computed, onBeforeMount, onMounted, onUpdated, onBeforeUpdate, provide } from 'vue'
+export default {
+  // setup在beforeCreate之前，所以不能访问到data、computed、methods和refs
+  // 只能访问以下property： props,attrs,slots,emits
+  setup() {
+    // ! 通过ref定义响应式变量
+    const count = ref(0)
+    function changeCount() {
+      // 用.value访问
+      count.value++
+
+    }
+    const msg = ref('hello-Vue3')
+    function changeMsg() {
+      msg.value = 'hello-Vite'
+    }
+    const reverseMsg = computed(() => {
+      return msg.value.split('').reverse().join('')
+    })
+    // reverseMsg 还一个对象，要用value取值
+    console.log("🚀 ~ file: App.vue ~ line 22 ~ reverseMsg ~ reverseMsg", reverseMsg)
+    console.log("🚀 ", reverseMsg.value)
+
+    // 通过reactive定义响应式对象
+    const person = reactive({
+      name: 'shenzeshu',
+      age: 27,
+      child: {
+        name: 'dog',
+        age: 12,
+        reverseMsg: computed(() => {
+          return msg.value.split('').reverse().join('')
+        })
+      }
+    })
+    function changePerson() {
+      person.name = 'dongleifei'
+      person.age = 28
+    }
+    watch(count, (newVal, oldVal) => {
+      console.log("🚀 ~ file: App.vue ~ line 29 ~ watch ~ oldVal", oldVal)
+      console.log("🚀 ~ file: App.vue ~ line 29 ~ watch ~ newVal", newVal)
+    })
+    // 无法监听对象
+    watch(person, (newVal, oldVal) => {
+      console.log("🚀 ~ file: App.vue ~ line 32 ~ watch ~ oldVal", oldVal) // 'dongleifei'
+      console.log("🚀 ~ file: App.vue ~ line 32 ~ watch ~ newVal", newVal) // 'dongleifei' 一样
+    })
+    // watchEffect(cb) 不需要指定监听的属性，自动收集依赖； 且初始化时会执行一次 callback
+    watchEffect(() => {
+      console.log(person.name);
+    })
+
+    onBeforeMount(() => {
+      console.log('OnBeforeMount');
+    })
+    //         provide --- inject 跨组件通信 ///////
+    provide('name', '张三') // 父组件传值(Content组件接收)
+  
+    // 加入通过ES6的扩展运算符...进行解构会使得对象的属性不是 响应式的！
+    // ---> toRefs(obj) 使得结构后的属性重新获得响应式 const {name, child} = toRefs(person）
+
+    return {
+      count,
+      changeCount,
+      msg,
+      changeMsg,
+      reverseMsg,
+      person,
+      ...toRefs(person),
+      changePerson
+    }
+
+  },
+  methods: {
+    injectNum(num) {
+      console.log(num);
+    }
+  },
+  mounted() {
+   
+    console.log("获取expose出的内容",  this.$refs.content.num);
+
+  },
+  components: {
+    Content,
+    HelloWorld
+  }
+}
+</script>
+
+
+<template>
+  <div>
+    <!-- 从 setup 返回的 refs 在模板中访问时是被自动浅解包的 -->
+    <p>{{ count }}</p> <button :style="{ background: '#ccc' }" @click="changeCount">+1</button>
+    <p>{{ person.name }}:{{ person.age }}</p> <button :style="{ background: '#ccc' }" @click="changePerson">换人</button>
+    <p>{{ child.name }}:{{ child.age }}</p>
+    <h3>{{ msg }}</h3> <button :style="{ background: '#ccc' }" @click="changeMsg">changeMsg</button>
+    <h3>{{ reverseMsg }}</h3>
+    <Content ref="content" :message="person.name" class="hahaha" id="hehehehe" @injectNum='injectNum' />
+
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ learnVue3/hello-vue3/src/components/Content.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <h2>content组件</h2>
+        <button @click="send2Parent">发射数据</button>
+        <h3>获取父组件provide的数据为: {{name}}</h3>
+    </div>
+</template>
+<script>
+import { onUpdated, toRefs, ref, inject } from 'vue'
+export default {
+    props: {
+        message: {
+            type: String,
+            default: "您好"
+        }
+    },
+    setup(props, context) { // 或者 setup(props, { expose, attrs, emit, slots}){}
+        console.log('content的获得属性', props);
+        console.log('setup中的context', context);
+        console.log('setup中的context', context.attrs);
+        console.log('setup中的context', context.attrs.class);
+        const num = ref('12345678')
+        function send2Parent() {
+            context.emit('injectNum', num.value)
+        }
+        // 变成响应式
+        const { message } = toRefs(props)
+        onUpdated(() => {
+            console.log('content中的', message);
+        })
+        context.expose({ send2Parent, num }) // 暴露出外界需要的属性、方法
+
+        // inject 接受数据
+        const name = inject('name')
+        return { send2Parent, name }
+        // return ()=> h('h2', num.value) // 如果return这一句，只会出现<h2>12345678</h2>,其他模板中的元素都没了。这时候想要
+        // 还是能让组件暴露出一些属性方法，就需要context.expose({})
+    }
+}
+</script>
